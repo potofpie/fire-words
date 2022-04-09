b3d711ad6c6c4d4d9570aa709ee3d9010c4772f8
Bobby Christopher <bobby@ixisdigital.com>
2022-04-08 23:01:17 -0400

    added tutorial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,127 @@
 import {FC }from 'react';
 
-import { GameBoard, StartScreen } from './components'
+import { GameBoard, StartScreen, TutorialGameBoard } from './components'
 import { useGameData} from './context/gameDataContext'
 import {  Credit } from './styled'
+import { TourProvider } from '@reactour/tour'
 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
+  Navigate,
+  useNavigate
 } from "react-router-dom";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 
 
+const createSteps = () => {
 
-export const App:FC = () => {
-  const  { gameBoardState } = useGameData()!
+  const steps = [
+    {
+      selector: '#root',
+      content: 'Welcome to fire words!',
+      padding: 0,
+      disableActions: false
+    },
+    {
+      selector: '[data-tut="11"]',
+      content: 'Select R!',
+      padding: 0
+    },
+    {
+      selector: '[data-tut="21"]',
+      content: 'Select U!'
+    },
+    {
+      selector: '[data-tut="31"]',
+      content: 'Select N!'
+    },
+    {
+      selector: '[data-tut="selected_word"]',
+      content: 'Submit the word you have selected!'
+    },
+    {
+      selector: '[data-tut="44"]',
+      content: 'Now select S!'
+    },
+    {
+      selector: '[data-tut="45"]',
+      content: 'and now O!'
+    },
+    {
+      selector: '[data-tut="selected_word"]',
+      content: 'Now try yo submit SO'
+    },
+    {
+      selector: '#root',
+      content: 'As you can see this word was rejected because 2 letter words dont count!'
+    },
+    {
+      selector: '[data-tut="21"]',
+      content: 'Last peice of info, The blue tiles are cooler'
+    },
+    {
+      selector: '[data-tut="14"]',
+      content: 'And the red tiles are hotter. Use the red tiles first because if they are to hot it will be GAME OVER!'
+    },
+    {
+      selector: '[data-tut="14"]',
+      content: 'You are ready to start! Press the X up there ^'
+    },
+  ]
+  return steps
+
+}
+
+export const TutorialRoute:FC = () => {
+  const disableBody = (target: any) => disableBodyScroll(target);
+  const enableBody = (target: any) => enableBodyScroll(target);
+  let navigate = useNavigate();
+
+
   return (
-    <>
-      <Router>
-        <Routes>
-          
-          <Route path='/' element={<StartScreen/>}/>
-          
-          <Route path='/game' element={ gameBoardState ? <GameBoard /> : <></>}/>
-          <Route path='/credits' element={ 
-                <div style={{display: 'flex' , flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-                Credits:
-                  <Credit href="https://www.flaticon.com/free-icons/letters-abc" title="letters abc icons">Letters abc icons created by mynamepong - Flaticon</Credit>
-                  <Credit href="https://www.macworld.com/article/196032/firewords.html" title="letters abc icons">This game was base on the orginal Fire Words</Credit>
-                </div>
-          } />
-              <Route
-                path="*"
-                element={<Navigate to="/" />}
-            />
+    <TourProvider 
+    steps={createSteps()} 
+    afterOpen={disableBody} 
+    beforeClose={enableBody}
+    onClickClose={() => navigate('game')}
+    showBadge={false}
+    showCloseButton={true}
+  >
+  <TutorialGameBoard /> 
+</TourProvider>
+  )
 
-        </Routes>
-      </Router>
+}
 
-    </>
-  );
+  export const App:FC = () => {
+    const  { gameBoardState } = useGameData()!
+
+    return (
+      <>
+        <Router>
+          <Routes>
+            
+            <Route path='/' element={<StartScreen/>}/>
+            <Route path='/tutorial' element={ gameBoardState ? <TutorialRoute/> : <></>}/>
+            <Route path='/game' element={ gameBoardState ? <GameBoard /> : <></>}/>
+            <Route path='/credits' element={ 
+                  <div style={{display: 'flex' , flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+                  Credits:
+                    <Credit href="https://www.flaticon.com/free-icons/letters-abc" title="letters abc icons">Letters abc icons created by mynamepong - Flaticon</Credit>
+                    <Credit href="https://www.macworld.com/article/196032/firewords.html" title="letters abc icons">This game was base on the orginal Fire Words</Credit>
+                  </div>
+            } />
+                <Route
+                  path="*"
+                  element={<Navigate to="/" />}
+              />
+
+          </Routes>
+        </Router>
+
+      </>
+    );
 }
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,5 +7,5 @@ export const SHORT_COLUMN_COUNT = Array.from(Array(6).keys())
 export const LONG_COLUMN_INDEXES = [1,3]
 export const tempColors = [ 
     ...chroma.scale(['#c4d8e2','#f2f0e6']).mode('lch').colors(10), 
-    ...chroma.scale(['#f2f0e6','#e8ccd7']).mode('lch').colors(10) 
+    ...chroma.scale(['#f2f0e6','#CB3434']).mode('lch').colors(10) 
 ]
